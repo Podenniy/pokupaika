3949f8f38d3acdb8de8feff2e2a598ba88167959
Dmitry <budyachocheck@gmail.com>
2013-07-26 12:58:27 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pokupaika::Application.config.secret_key_base = 'ee6f945134ff76a3ae3775436fb129183258ded31f2d528a60fd389c2e1480d12cb3141e4a9698b6e1dab5b6d21f890a19d51b6ee86047264ba2a1a97f434758'
