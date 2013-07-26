d86d2155e57c45da2d3d25d1f6a8d6d51667c0e3
Dmitry <budyachocheck@gmail.com>
2013-07-26 17:31:49 +0300

    Scaffold LineItem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,18 @@ class ApplicationController < ActionController::Base
 
   private
 
-    def curent_cart
+    def current_cart
       Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
       cart = Cart.create
       session[:cart_id] = cart.id
       cart
+    end
+
+    def increment_counter
+      if session[:counter].nil?
+        session[:counter] =0
+      end
+      session[:counter] +=1
     end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,6 @@ class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
   	@time = Time.now
+  	@count = increment_counter
   end
 end
